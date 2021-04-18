ba0e203938b588476cd5e53a82daf111a592b87d
John Harrington <harrington.john@husky.neu.edu>
2021-04-18 18:37:47 -0400

    Added windowed average plotter

--- visualizations.py
+++ visualizations.py
@@ -1,8 +1,8 @@
 import datetime as dt
 import matplotlib.pyplot as plt
+import numpy as np
 
-
-def get_ave_episode_rewards(ave_episode_dict):
+def get_ave_episode_rewards(ave_episode_dict, figure_title='testing_agents'):
 
     fig, ax = plt.subplots()
 
@@ -13,12 +13,20 @@ def get_ave_episode_rewards(ave_episode_dict):
     ax.set_xlabel('Episodes')
     ax.set_ylabel('Average Reward')
     ax.set_title('Average Reward over time for Overcooked Agents')
-
-    plt.savefig('testing_agents.png')
+    plt.legend(loc='upper right')
+    plt.savefig(f'{figure_title}.png')
 
     return fig
 
-#test_dict = {'central':[0,1,1,1,0,10], 'decentral':[0,0,1,1,1,2,3]}
-#get_ave_episode_rewards(test_dict)
-#plt.savefig('filename')
-#lt.show()
+
+def windowed_average_plot(ave_episode_dict, window_size=10, figure_title='testing_agents'):
+
+    windowed = {}
+    for plot_title, rewards in ave_episode_dict.items():
+
+        window = np.ones(int(window_size))/float(window_size)
+        windowed[plot_title] =  np.convolve(rewards, window, 'same')
+
+    fig = get_ave_episode_rewards(windowed, figure_title)
+
+    return fig 
